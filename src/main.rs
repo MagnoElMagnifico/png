aace5c909432d73e91591d00907c7a0a8f1488a0
MarcosG <marcosgranja317@gmail.com>
2023-01-17 00:02:55 +0100

    read a binary file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::{env, fs, path::Path, process};
+
 fn main() {
-    println!("Hello, world!");
+    let mut args = env::args();
+    let program_name = args.next().expect("Program name not found");
+
+    let _ = match args.next() {
+        None => {
+            println!("USAGE: {} <filepath.png>", program_name);
+            process::exit(1);
+        }
+
+        Some(file_param) => match fs::read(Path::new(&file_param)) {
+            Err(error) => {
+                println!("ERROR: {}", error);
+                process::exit(1);
+            }
+            Ok(file_data) => file_data,
+        }
+    };
 }
