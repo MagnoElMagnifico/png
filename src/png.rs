3b0b140e10805d0dfb4221af552d291e073f8552
MarcosG <marcosgranja317@gmail.com>
2023-01-24 22:03:42 +0100

    Fix CRC

--- src/png.rs
+++ src/png.rs
@@ -34,7 +34,7 @@ impl Png {
 
         let mut png = Self {
             chunks: Vec::with_capacity(3),
-            crc: Crc::new(), // Compute CRC as well
+            crc: Crc::new(),
         };
 
         let mut should_end = false;
@@ -43,8 +43,16 @@ impl Png {
             let chunk_bytes = &file_data[p + 4..(p + 4) + 4 + chunk_size + 4];
             let chunk = Chunk::from_bytes(chunk_size, chunk_bytes);
 
-            // TODO: this fails
-            // assert_eq!(chunk.crc, png.crc.calculate(chunk_bytes));
+            let calculated_crc = png.crc.calculate(&chunk_bytes[..chunk_bytes.len() - 4]);
+            if calculated_crc == chunk.crc {
+                return Err(io::Error::new(
+                    io::ErrorKind::InvalidData,
+                    format!(
+                        "The CRCs do not match: read {}, calculated {}",
+                        chunk.crc, calculated_crc
+                    ),
+                ));
+            }
 
             p += chunk_size + 12;
 
@@ -76,7 +84,7 @@ impl Png {
 }
 
 const CRC_MASK: u32 = 0xEDB88320;
-const CRC_TABLE_SZ: usize = 256;
+const CRC_TABLE_SZ: usize = u8::MAX as usize + 1;
 
 /// A Cyclic redundancy check (CRC) is an error-detecting code. Blocks of data entering these
 /// systems get a short check value attached, based on the remainder of a polynomial division of
@@ -104,6 +112,53 @@ const CRC_TABLE_SZ: usize = 256;
 /// Thus the coefficients are (1 - 32, ignoring 32): 1110 1101 1011 1000 1000 0110 0100 0000
 /// which is exactly EBD88320 in hex.
 ///
+/// # Example
+///
+/// In this example, we shall encode 14 bits of message with a 3-bit CRC, with a polynomial `x^3 + x
+/// + 1` (coefficients 1011). Start with the message to be encoded: `11 0100 1110 1100` and execute
+/// a bitwise XOR:
+///
+/// ```
+/// 11010011101100 000 <--- input right padded by 3 bits
+/// 1011               <--- divisor
+/// 01100011101100 000 <--- result (note the first four bits are the XOR with the divisor beneath, the rest of the bits are unchanged)
+///  1011              <--- divisor ...
+/// 00111011101100 000
+///   1011
+/// 00010111101100 000
+///    1011
+/// 00000001101100 000 <--- note that the divisor moves over to align with the next 1 in the dividend (since quotient for that step was zero)
+///        1011             (in other words, it doesn't necessarily move one bit per iteration)
+/// 00000000110100 000
+///         1011
+/// 00000000011000 000
+///          1011
+/// 00000000001110 000
+///           1011
+/// 00000000000101 000
+///            101 1
+/// -----------------
+/// 00000000000000 100 <--- remainder (3 bits).  Division algorithm stops here as dividend is equal to zero.
+/// ```
+///
+/// Now, to check the validity of the message, the operation will be repeated with the remainder
+/// instead of zeroes. It should equal zero if there are no detectable errors.
+///
+/// ```
+/// 11010011101100 100 <--- input with check value
+/// 1011               <--- divisor
+/// 01100011101100 100 <--- result
+///   ...
+/// 00000000000101 100
+///            101 1
+/// ------------------
+/// 00000000000000 000 <--- remainder
+/// ```
+///
+/// A practical algorithm for the CRC-32 variant of CRC is the CRCTable, which is a memoization
+/// (storage of all the possibilities -- 256) of a calculation that would have to be repeated for
+/// each byte of the message.
+///
 /// Source (modified): https://en.wikipedia.org/wiki/Cyclic_redundancy_check
 ///
 /// Translated from the C code avaliable here:
@@ -116,36 +171,33 @@ impl Crc {
     pub fn new() -> Self {
         let mut table = [0; CRC_TABLE_SZ];
 
-        for (i, element) in table.iter_mut().enumerate() {
-            let mut c = i as u32;
+        for (i, table_byte) in table.iter_mut().enumerate() {
+            let mut byte = i as u32;
             for _ in 0..8 {
-                if (c & 1) == 1 {
-                    c = CRC_MASK ^ (c >> 1);
+                if (byte & 1) == 1 {
+                    byte = CRC_MASK ^ (byte >> 1);
                 } else {
-                    c >>= 1;
+                    byte >>= 1;
                 }
             }
 
-            *element = c;
+            *table_byte = byte;
         }
 
         Crc(table)
     }
 
-    fn update_crc(&self, crc: u32, buffer: &[u8]) -> u32 {
-        let mut c = crc;
-
-        for n in buffer {
-            c = self.0[(c as u8 ^ n) as usize] ^ (c >> 8);
-        }
-
-        c
-    }
-
     /// Returns the CRC of the bytes on buffer.
     pub fn calculate(&self, buffer: &[u8]) -> u32 {
-        // 1's complement of update_crc
-        self.update_crc(0xffff_ffff_u32, buffer) ^ 0xffff_ffff_u32
+        let mut crc = 0xFFFF_FFFF_u32;
+
+        for byte in buffer {
+            let index = crc as u8 ^ byte;
+            crc = (crc >> 8) ^ self.0[index as usize];
+        }
+
+        // Invert the bits (1's complement)
+        crc ^ 0xFFFF_FFFF_u32
     }
 }
 
@@ -174,7 +226,13 @@ impl Chunk {
     }
 
     pub fn from_bytes(size: usize, data: &[u8]) -> Self {
-        assert_eq!(size + 8, data.len());
+        assert_eq!(
+            size + 8,
+            data.len(),
+            "The data length should be {}, got {}",
+            size + 8,
+            data.len()
+        );
 
         Self {
             chunk_type: ChunkCode::from_slice(&data[..4]).unwrap(),
@@ -213,14 +271,20 @@ pub struct ChunkCode([u8; 4]);
 
 impl ChunkCode {
     pub fn from_code(code: &str) -> Self {
-        let mut chars = code.chars();
+        assert_eq!(
+            4,
+            code.len(),
+            "The code length should be 4, got {}",
+            code.len()
+        );
 
-        ChunkCode([
-            chars.next().unwrap() as u8,
-            chars.next().unwrap() as u8,
-            chars.next().unwrap() as u8,
-            chars.next().unwrap() as u8,
-        ])
+        let mut chunk_code = [0; 4];
+
+        for (i, char) in code.chars().enumerate() {
+            chunk_code[i] = char as u8;
+        }
+
+        ChunkCode(chunk_code)
     }
 
     pub fn from_slice(data: &[u8]) -> Result<Self, std::array::TryFromSliceError> {
