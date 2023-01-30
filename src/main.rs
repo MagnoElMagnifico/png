1dc7070e1c3849677c750a802623eea969d9b77f
MarcosG <marcosgranja317@gmail.com>
2023-01-30 23:37:44 +0100

    Create some tests

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,55 @@
-use std::{env, path::Path, process};
-
 mod chunks;
 mod crc;
+mod filter;
 mod png;
 
-use crate::chunks::{ChunkType, GenericChunk};
+use std::{env, path::Path, process::exit};
 use crate::png::Png;
 
 fn main() {
     let mut args = env::args();
     let program_name = args.next().expect("Program name not found");
 
-    let mut png = match args.next() {
+    let png = match args.next() {
         None => {
             println!("USAGE: {} <filepath.png>", program_name);
-            process::exit(1);
+            exit(1);
         }
 
         Some(file_param) => match Png::read(Path::new(&file_param)) {
             Err(error) => {
                 println!("ERROR: {}", error);
-                process::exit(1);
+                exit(1);
             }
             Ok(png) => png,
         },
     };
 
     println!("{:#?}", png.chunks);
-    png.chunks.insert(
-        1,
-        Box::new(GenericChunk::new(
-            ChunkType::from_code("teSt"),
-            "*secret code here*".as_bytes(),
-        )),
-    );
-    png.write(Path::new("./output.png")).unwrap();
+}
+
+// These tests have to be checked manually opening the PNG output for the moment.
+#[cfg(test)]
+mod tests {
+    use std::path::Path;
+    use crate::chunks::{IDAT, ImageTrailer, GenericChunk, ImageHeader};
+    use crate::png::Png;
+
+    #[test]
+    fn open_png() {
+        todo!();
+    }
+
+    #[test]
+    fn create_png() {
+        let mut png = Png::empty();
+
+        png.chunks.push(Box::new(ImageHeader::new((1, 1), 8, 2, false)));
+        png.chunks.push(Box::new(GenericChunk::from_bytes(IDAT, &[
+                                                          0, 0, 0
+        ])));
+        png.chunks.push(Box::new(ImageTrailer));
+
+        png.write(Path::new("generated.png"));
+    }
 }
