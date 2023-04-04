057c69c4c4683f020dcd69ca4372f7608c875be0
MarcosG <marcosgranja317@gmail.com>
2023-04-05 00:05:14 +0200

    Add sawtooth wave generator

--- src/main.rs
+++ src/main.rs
@@ -21,6 +21,7 @@ fn main() {
 
         "sin-wav" => create_sinwav(&file_name),
         "sqr-wav" => create_sqrwav(&file_name),
+        "saw-wav" => create_sawwav(&file_name),
         _ => println!("Unknown option: {}", file_type),
     }
 }
@@ -30,12 +31,12 @@ use std::f32::consts::PI;
 fn create_sinwav(file_name: &str) {
     let sample_rate = 44100;
     let seconds = 5;
-    let frecuency = 100.0;
+    let frecuency = 220.0;
 
     let volume = 64.0;
     let volume_offset = 128.0;
 
-    let mut data = vec![0_u8; seconds * sample_rate as usize];
+    let mut data = vec![0; seconds * sample_rate as usize];
 
     for (i, sample) in data.iter_mut().enumerate() {
         let t = i as f32 / sample_rate as f32;
@@ -50,19 +51,43 @@ fn create_sinwav(file_name: &str) {
 fn create_sqrwav(file_name: &str) {
     let sample_rate = 44100;
     let seconds = 5;
+    let frecuency = 100.0;
+    let samples_per_period = sample_rate as f32 / frecuency;
 
     let volume = 32000;
-    let period = 1.0 / 100.0;
-    let samples_per_period = sample_rate as f32 * period;
-    let high_samples = 0.5 * samples_per_period;
+    let pulse_width = 0.5 * samples_per_period;
 
-    let mut data = vec![0_i16; seconds * sample_rate as usize];
+    let mut data = vec![0; seconds * sample_rate as usize];
 
     for (i, sample) in data.iter_mut().enumerate() {
-        *sample = volume * (i as f32 % samples_per_period < high_samples) as i16;
+        let x = (i % samples_per_period as usize) < pulse_width as usize;
+        *sample = volume * x as i16;
     }
 
     Wav::from_data(WavSamples::Mono16(data), sample_rate)
         .write(Path::new(&file_name))
         .expect("error writing WAV file");
 }
+
+fn create_sawwav(file_name: &str) {
+    let sample_rate = 44100;
+    let seconds = 5;
+    let frecuency = 100.0;
+    let samples_per_period = sample_rate as f32 / frecuency;
+
+    let volume = 64.0;
+    let volume_offset = 128.0;
+
+    let mut data = vec![(0, 0); seconds * sample_rate as usize];
+
+    for (i, (sample, _)) in data.iter_mut().enumerate() {
+        // x is the porcentage of the wave at the current point
+        let x = (i % samples_per_period as usize) as f32 / samples_per_period as f32;
+        *sample = (volume_offset + volume * x) as u8;
+    }
+
+    Wav::from_data(WavSamples::Stereo8(data), sample_rate)
+        .write(Path::new(&file_name))
+        .expect("error writing WAV file");
+}
+
