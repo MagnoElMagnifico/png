2dc05cdd2d67b71963d26a726f3d7dc1e9d1fbe5
MarcosG <marcosgranja317@gmail.com>
2023-04-04 12:20:16 +0200

    Use from command line Wav or Png

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,25 @@
+use png::Wav;
 use png::{Png, IDAT};
 use std::{env::args, path::Path};
 
 // Create a simple PNG image
 fn main() {
-    let file = args().nth(1).unwrap();
-    let png = Png::read(Path::new(&file)).unwrap();
+    let file_type = args().nth(1).unwrap();
+    let file_name = args().nth(2).unwrap();
 
-    for idat in png.chunks.iter().filter(|x| x.get_type() == IDAT) {
-        println!("{:?}", idat);
+    match &file_type[..] {
+        "png" => {
+            let png = Png::read(Path::new(&file_name)).unwrap();
+            for idat in png.chunks.iter().filter(|x| x.get_type() == IDAT) {
+                println!("{:?}", idat);
+            }
+        }
+
+        "wav" => {
+            let wav = Wav::read(Path::new(&file_name)).unwrap();
+            println!("{:?}", wav.data);
+        }
+
+        _ => println!("Unknown option: {}", file_type),
     }
 }
