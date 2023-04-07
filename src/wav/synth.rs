36c1444cae89f53f8eb0c43ba235112e486bc88e
MarcosG <marcosgranja317@gmail.com>
2023-04-07 23:20:53 +0200

    Make Oscillator a trait
    - Now the user can implement more complex Oscillators by themselves.
    - This change allows to add different types of Oscillators into a
      Compound Oscillator.

--- src/wav/synth.rs
+++ src/wav/synth.rs
@@ -1,27 +1,36 @@
 use crate::{Wav, WavSamples};
 use std::f32::consts::TAU;
 
+pub trait Oscillator {
+    fn get_samples(&self, time: u32) -> WavSamples;
+    fn get_sample_rate(&self) -> u32;
+
+    fn to_wav(&self, time: u32) -> Wav {
+        Wav::from_data(self.get_samples(time), self.get_sample_rate())
+    }
+
+    fn weighted_sum(self, other: Self) -> Self;
+}
+
 pub enum WaveForm {
     Sin,
     Sqr(f32),
     Saw,
-    Custom(fn(usize, &Oscillator) -> u8),
+    Custom(fn(usize, &BasicOscillator) -> u8),
 }
 
-pub struct Oscillator {
+pub struct BasicOscillator {
     wave: WaveForm,
-    pub sample_rate: u32,
-    pub spp: f32,
+    sample_rate: u32,
     pub frecuency: f32,
     pub volume: u8,
     pub offset: u8,
 }
 
-impl Oscillator {
+impl BasicOscillator {
     pub fn new(sample_rate: u32, frecuency: f32, wave: WaveForm, volume: u8, offset: u8) -> Self {
         Self {
             sample_rate,
-            spp: sample_rate as f32 / frecuency,
             frecuency,
             wave,
             volume,
@@ -29,21 +38,8 @@ impl Oscillator {
         }
     }
 
-    pub fn get_samples(&self, time: u32) -> WavSamples {
-        let mut data = vec![0; (time * self.sample_rate / 1000) as usize];
-
-        match self.wave {
-            WaveForm::Sin => self.sin_wave(&mut data),
-            WaveForm::Sqr(pulse_width) => self.sqr_wave(&mut data, pulse_width),
-            WaveForm::Saw => self.saw_wave(&mut data),
-            WaveForm::Custom(callback) => self.custom_wave(&mut data, callback),
-        }
-
-        WavSamples::Mono8(data)
-    }
-
-    pub fn to_wav(&self, time: u32) -> Wav {
-        Wav::from_data(self.get_samples(time), self.sample_rate)
+    pub fn get_spp(&self) -> f32 {
+        self.sample_rate as f32 / self.frecuency
     }
 
     fn sin_wave(&self, data: &mut [u8]) {
@@ -58,8 +54,8 @@ impl Oscillator {
     }
 
     fn sqr_wave(&self, data: &mut [u8], pulse_width: f32) {
-        let spp = self.spp as usize;
-        let high_samples = (pulse_width * self.spp) as usize;
+        let spp = self.get_spp() as usize;
+        let high_samples = (pulse_width * self.get_spp()) as usize;
 
         for (i, sample) in data.iter_mut().enumerate() {
             let x = i % spp < high_samples;
@@ -70,14 +66,37 @@ impl Oscillator {
     fn saw_wave(&self, data: &mut [u8]) {
         for (i, sample) in data.iter_mut().enumerate() {
             // x is the porcentage of the wave at the current point
-            let x = (i % self.spp as usize) as f32 / self.spp as f32;
+            let x = (i % self.get_spp() as usize) as f32 / self.get_spp() as f32;
             *sample = self.offset + self.volume * x as u8;
         }
     }
 
-    fn custom_wave(&self, data: &mut [u8], callback: fn(usize, &Oscillator) -> u8) {
+    fn custom_wave(&self, data: &mut [u8], callback: fn(usize, &BasicOscillator) -> u8) {
         for (i, sample) in data.iter_mut().enumerate() {
             *sample = callback(i, self);
         }
+    }
+}
+
+impl Oscillator for BasicOscillator {
+    fn get_samples(&self, time: u32) -> WavSamples {
+        let mut data = vec![0; (time * self.sample_rate / 1000) as usize];
+
+        match self.wave {
+            WaveForm::Sin => self.sin_wave(&mut data),
+            WaveForm::Sqr(pulse_width) => self.sqr_wave(&mut data, pulse_width),
+            WaveForm::Saw => self.saw_wave(&mut data),
+            WaveForm::Custom(callback) => self.custom_wave(&mut data, callback),
+        }
+
+        WavSamples::Mono8(data)
+    }
+
+    fn get_sample_rate(&self) -> u32 {
+        self.sample_rate
+    }
+
+    fn weighted_sum(self, _other: Self) -> Self {
+        todo!();
     }
 }
